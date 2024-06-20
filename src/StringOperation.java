866161ca51f1ded04224f11fa74b8120da4f20a8
Madhusudhanam37 <madhusudhanam37@gmail.com>
2024-06-20 22:49:29 +0530

    Day 2 task

--- src/StringOperation.java
+++ src/StringOperation.java
@@ -29,33 +29,39 @@ public class StringOperation {
     }
 
     public void stringPreDefinedStripMethod(String str5){
-
+        System.out.println(str5.strip());
     }
 
     public void stringPreDefinedSubStringMethod(String str5){
-
+        String sss=str5.substring(11);
+        System.out.println(sss);
+        System.out.println("-----------------------------------");
+        String ssss=str5.substring(0,10);
+        System.out.println(ssss);
     }
 
 
     public static void main(String[] args) {
-//        Scanner scanner=new Scanner(System.in);
-//        System.out.println("Enter the str1 : ");
-//        String str1=scanner.next();
-//        System.out.println("Enter the str2 : ");
-//        String str2=scanner.next();
-//        System.out.println("Enter the str3 : ");
-//        String str3=scanner.next();
-//        System.out.println("Enter the str4 : ");
-//        String str4=scanner.next();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the str1 : ");
+        String str1=scanner.next();
+        System.out.println("Enter the str2 : ");
+        String str2=scanner.next();
+        System.out.println("Enter the str3 : ");
+        String str3=scanner.next();
+        System.out.println("Enter the str4 : ");
+        String str4=scanner.next();
 //        System.out.println("Enter the str5 : ");
 //        String str5=scanner.nextLine();
 
         StringOperation stringOperation = new StringOperation();
-//        boolean s=stringOperation.nameEquals(str1,str2);
-//        System.out.println("Compare using Equals : "+s);
-//        System.out.println("Compare using EqualsIgnoreCase : "+stringOperation.nameEqualsIgnoreCase(str3,str4));
+        boolean s=stringOperation.nameEquals(str1,str2);
+        System.out.println("Compare using Equals : "+s);
+        System.out.println("Compare using EqualsIgnoreCase : "+stringOperation.nameEqualsIgnoreCase(str3,str4));
 //        stringOperation.stringPreDefinedSplitMethod(str5);
         stringOperation.stringPreDefinedSplitMethod("Learning Software Testing Class");
         stringOperation.stringPreDefinedTrimMethod("Software Training");
+        stringOperation.stringPreDefinedStripMethod("\u2002\u2002Hello World\u2002\u2002");
+        stringOperation.stringPreDefinedSubStringMethod("Automation Testing");
     }
 }
