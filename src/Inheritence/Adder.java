b06144a812c5304c80b1736dd7b0b1505926e17a
Madhusudhanam37 <madhusudhanam37@gmail.com>
2024-06-26 11:52:16 +0530

    Day 5 task

---
+++ src/Inheritence/Adder.java
@@ -0,0 +1,9 @@
+package Inheritence;
+
+public class Adder extends Arithmetic{
+
+    public static void main(String[] args) {
+        Adder adder=new Adder();
+        adder.add(10,20,30);
+    }
+}
---
+++ src/Inheritence/Arithmetic.java
@@ -0,0 +1,9 @@
+package Inheritence;
+
+public class Arithmetic {
+
+    public int add(int a,int b,int c){
+
+        return a+b+c;
+    }
+}
